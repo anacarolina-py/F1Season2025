916c48f8f156bed12f97d01b3e47571c9b4c436d
anacarolina-py <anacarolinaalvez_@hotmail.com>
2025-12-21 11:00:46 -0300

    feat: general changes, saving bd ok

--- Domain.Engeneering.Models/DTOs/CarStatusDTO.cs
+++ Domain.Engeneering.Models/DTOs/CarStatusDTO.cs
@@ -2,7 +2,7 @@
 
 public class CarStatusDTO
 {
-    public int CarId { get; set; }
+    public int Id { get; set; }
     public decimal AerodynamicCoefficient { get; set; }
-    public decimal PowerCoefficent { get; set; }    
+    public decimal PowerCoefficient { get; set; }    
 }
